a1ae18f44a10928158dd3043796dd3410346c18e
th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2023-01-17 13:55:01 +0530

    rewrite

--- setup.sh
+++ setup.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 #script for  settting up termux
-FOO='alias wps="sudo python tools/OneShot/oneshot.py -i wlan0 --iface-down -K"'
-
+FOO='alias ls="ls -al"'
+DWN='alias dwn="cd storage/downloads"'
 tmux_up() 
    {
     apt update -y && apt upgrade -yy && pkg i root-repo &&            
-    pkg i git curl zsh tsu python wpa-supplicant pixiewps iw openssl neofetch python -y && clear && echo "installing yt-dlp" && python3 -m pip install yt-dlp && omz ;  
-    }
-#INSTALLING OH MY ZSH                
-
-omz(){ 
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ; 
-	chsh -s zsh ;
-	rm $HOME/../usr/etc/motd ;
-	echo 'neofetch'>> $HOME/.zshrc ;
-	install_wps ;  
-	}
-
-#INSTALLING WPS BRUTEFORCE
-
-install_wps() {      
-    cd $HOME ; mkdir tools ; cd tools ;
-    curl -sSf https://raw.githubusercontent.com/drygdryg/OneShot_Termux_installer/master/installer.sh | bash ; 
-    echo "installed wps pin brute " ;
-    echo $FOO>> $HOME/.zshrc ;
-    termux-reload-settings ; 
+    pkg i git curl zsh tsu wget  openssl neofetch python -y && clear && echo "installing yt-dlp" && python3 -m pip install yt-dlp   
     }
     
  tmux_up
+ echo $FOO>> $HOME/.zshrc ;
+ echo $DWN>> $HOME/.zshrc ;
+ 
+ 
