80424382cf4f2a5bac46231ba71558297054bd51
th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2023-01-19 13:13:09 +0530

    adding .properties

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 #script for  settting up termux
  apt update -y && apt upgrade -y
  pkg i  pv 
- ( pkg i root-repo git curl zsh tsu wget  openssl neofetch python -y &&  echo "installing yt-dlp" && python3 -m pip install yt-dlp && cd "$HOME" &&  curl -fsSL https://raw.githubusercontent.com/th3cr00k3dm4n/termux-setup/master/.zshrc >> .zshrc &&  chsh -s zsh &&  rm ../usr/etc/motd ) | pv -l > /dev/null
+ ( pkg i root-repo openssh micro git curl zsh tsu wget openssl neofetch python -y &&  echo "installing yt-dlp" && python3 -m pip install yt-dlp ) | pv -l > /dev/null
+ (  cd "$HOME" &&  curl -fsSL https://raw.githubusercontent.com/th3cr00k3dm4n/termux-setup/master/.zshrc >> .zshrc &&  chsh -s zsh &&  rm ../usr/etc/motd ) | pv -l > /dev/null
  sleep 0.3
  echo "+-+-+-+-+-+-+-+-+-+"
  echo "+ Finished Install +"
