7c26d1b6b5c7bb4b36c901c1e7aa6d6e690207fe
th3cr00k3dm4n <73489161+th3cr00k3dm4n@users.noreply.github.com>
2023-07-02 12:14:29 +0530

    fix

--- .resources/setup.sh
+++ .resources/setup.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 #script for  settting up termux
  termux-setup-storage
- apt update -y && apt upgrade -y
- pkg i  pv 
+ sleep 0.5
+ apt update -y && apt upgrade -y 
  clear
  sleep 0.5
- ( pkg i root-repo openssh subversion micro git curl zsh tsu wget openssl neofetch python -y  ) | pv -l > /dev/null
+ pkg i  openssh subversion micro git curl zsh wget openssl neofetch python ffmpeg -y  
+ sleep 0.5
+ clear
  echo ""
  echo ""
- echo "installing yt-dlp" 
- ( python3 -m pip install yt-dlp ) | pv -l > /dev/null
- ( cd "$HOME" &&  curl -fsSL https://raw.githubusercontent.com/th3cr00k3dm4n/termux-setup/master/.resources/.zshrc > .zshrc &&  chsh -s zsh &&  rm ../usr/etc/motd ) | pv -l > /dev/null
+ echo "installing yt-dlp"
+ sleep 0.5
+ clear 
+ python3 -m pip install yt-dlp
+ sleep 0.5 
+ cd "$HOME" &&  curl -fsSL https://raw.githubusercontent.com/th3cr00k3dm4n/termux-setup/master/.resources/.zshrc > .zshrc &&  chsh -s zsh &&  rm ../usr/etc/motd 
  sleep 0.3
  echo "*+ Installing fonts and properties +*"
  curl -fsSL https://raw.githubusercontent.com/th3cr00k3dm4n/termux-setup/main/.resources/font.ttf >> "$HOME"/.termux/font.ttf 
  curl -fsSL https://raw.githubusercontent.com/th3cr00k3dm4n/termux-setup/main/.resources/termux.properties >> "$HOME"/.termux/termux.properties 
  sleep 0.3
- ( svn export https://github.com/th3cr00k3dm4n/termux-setup/trunk/.resources/.zsh .zsh ) | pv -l > /dev/null
+ svn export https://github.com/th3cr00k3dm4n/termux-setup/trunk/.resources/.zsh .zsh
  sleep 0.3
  clear
  termux-reload-settings
@@ -24,4 +29,4 @@
  echo "+ Finished Install +"
  echo "+-+-+-+-+-+-+-+-+-+"
  sleep 2
-exit
+ exit
